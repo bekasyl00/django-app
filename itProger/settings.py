c57443951b813905c7fd2b5756ba65842adde603
gosha_dudar <admin@itproger.com>
2019-11-04 15:10:12 +0200

    Added PostgreSQL

--- itProger/settings.py
+++ itProger/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +126,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'user'
+
+django_heroku.settings(locals())
